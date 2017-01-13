2340b9608b9afe06185a6ff0580e843e170870b9
LAPTOP-JQO3IREF\moraj <morajhansi@gmail.com>
2017-01-12 19:33:39 -0600

    Submitting homework 12

---
+++ hw12-jan 11,2017/myScript.js
@@ -0,0 +1,7 @@
+
+function myFunction() {
+   document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+function myFunction1() {
+	document.getElementById("demo1").innerHTML="  Sum of 30 and 40 is 70";
+}

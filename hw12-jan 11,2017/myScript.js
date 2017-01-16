492cf4492d4f879b59af0af435f131c3d1e4596a
LAPTOP-JQO3IREF\moraj <morajhansi@gmail.com>
2017-01-15 19:20:54 -0600

    no message

--- hw12-jan 11,2017/myScript.js
+++ hw12-jan 11,2017/myScript.js
@@ -5,3 +5,21 @@ function myFunction() {
 function myFunction1() {
 	document.getElementById("demo1").innerHTML="  Sum of 30 and 40 is 70";
 }
+var a=2;
+console.log(typeof a);
+
+x=2|22;
+console.log(x);
+
+x=2&22;
+console.log(x);
+
+x=2&&22;
+console.log(x);
+
+x=2||22;
+console.log(x);
+
+
+x=22||2;
+console.log(x);
